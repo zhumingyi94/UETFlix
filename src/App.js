accfa6c303cd283b180452a6c7e54ae9382780af
nhatdm22022537 <dominhnhat2004@gmail.com>
2024-05-28 09:39:25 +0700

    Final front-end product

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
+import React, { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import MainText from './components/MainText';
 import TextInput from './components/TextInput';
 import RecommendationButton from './components/RecommendationButton';
+import moviesData from './movies.json';
+
+const API_KEY = 'd91022e8';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [recommendations, setRecommendations] = useState([]);
+  const [showRecommendations, setShowRecommendations] = useState(false);
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+    setShowRecommendations(false); // Clear recommendations on input change
+  };
+
+  const fetchMovieDetails = async (title, year) => {
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?t=${encodeURIComponent(title)}&y=${year}&apikey=${API_KEY}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching movie details:', error);
+      return null;
+    }
+  };
+
+  const handleButtonClick = async () => {
+    const user = moviesData.find((user) => user.user_id === parseInt(inputValue, 10));
+    if (user) {
+      const topMovies = user.movies.slice(0, 5);
+      const movieDetailsPromises = topMovies.map(movie => fetchMovieDetails(movie.Name, movie.Year));
+      const movieDetails = await Promise.all(movieDetailsPromises);
+      setRecommendations(movieDetails.filter(movie => movie && movie.Poster));
+    } else {
+      setRecommendations([]);
+    }
+
+    setShowRecommendations(true); 
+  };
+
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <MainText></MainText>
-      <TextInput></TextInput>
-      <RecommendationButton></RecommendationButton>
+      <NavBar />
+      <MainText />
+      <TextInput inputValue={inputValue} onInputChange={handleInputChange} />
+      <RecommendationButton onButtonClick={handleButtonClick} />
+
+      {showRecommendations && ( 
+        <div className="Recommendations">
+          <h2>Top 5 Movies</h2>
+          <ul>
+            {recommendations.map((movie, index) => (
+              <li key={index} className="movie-container">
+                <img src={movie.Poster} alt={movie.Title} className="movie-poster" />
+                <div className="movie-details">
+                  <h3>{movie.Title} ({movie.Year})</h3>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './TextInput.css';
 
-function TextInput({inputValue, onInputChange}) {
-    const handleChange = (e) => {
-        onInputChange(e.target.value);
-    };
+function TextInput({ inputValue, onInputChange }) {
+  const handleChange = (e) => {
+    onInputChange(e.target.value);
+  };
 
-    return (
-        <div className="TextInputContainer">
-            <input type="text" class="TextInput" placeholder="Type the genre you like" value={inputValue} onChange={handleChange}></input>
-        </div>
-    );
+  return (
+    <div className="TextInputContainer">
+      <input
+        type="text"
+        className="TextInput"
+        placeholder="Type the user_id you would like to get a recommend"
+        value={inputValue}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
 
 export default TextInput;
